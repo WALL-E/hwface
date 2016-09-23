1bbb07fab41d084da198661db96121f2244d0f97
zhangzheng <18601254750.163.com>
2016-09-23 14:10:59 +0800

    默认查询最近7天的考勤记录

--- src/background.js
+++ src/background.js
@@ -6,15 +6,23 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   chrome.tabs.executeScript({
-    code: 
-        'var Date = new Date(); ' + 
-        'var year = Date.getFullYear(); ' +
-        'var month = Date.getMonth(); ' +
-        'var day = Date.getDate(); ' +
+    code:
+        'var date = new Date((new Date).getTime() - 7 * 24 * 3600 * 1000); ' +
+        'var year = date.getFullYear(); ' +
+        'var month = date.getMonth(); ' +
+        'var day = date.getDate(); ' +
         'month = month + 1; month = month.toString().length === 1 && "0"+month || month; ' +
         'day = day.toString().length === 1 && "0"+day || day; ' +
-        'document.getElementById("startdate").setAttribute("value", year + "-" + month + "-" + day);' + 
+        'document.getElementById("startdate").setAttribute("value", year + "-" + month + "-" + day);' +
+        '' +
+        'date = new Date(); ' +
+        'year = date.getFullYear(); ' +
+        'month = date.getMonth(); ' +
+        'day = date.getDate(); ' +
+        'month = month + 1; month = month.toString().length === 1 && "0"+month || month; ' +
+        'day = day.toString().length === 1 && "0"+day || day; ' +
         'document.getElementById("enddate").setAttribute("value", year + "-" + month + "-" + day);' +
+        '' +
         'document.getElementById("button").click();' +
         'console.log("I love JS x10"); ' +
         ''
